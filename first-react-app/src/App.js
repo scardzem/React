9eaece566e0033302e9547ad7668e6b78a1b1c5d
scardzem <scardzem@naver.com>
2022-06-29 17:09:21 +0900

    first commit

---
+++ first-react-app/src/App.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+import ArrayRendering from './components/ArrayRendering';
+function App() {
+  return <ArrayRendering></ArrayRendering>;
+}
+
+export default App;
