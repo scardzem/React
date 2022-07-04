8df54aa55720145039bd99a81aa0431bae51ffab
scardzem <scardzem@naver.com>
2022-07-04 16:59:57 +0900

    07.04

--- todo-app-optimize/src/App.js
+++ todo-app-optimize/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -16,8 +16,26 @@ function createBultTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'CHECKED':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(createBultTodos);
+  // 초기상태를 undefined로 설정하고
+  // 세 번째 파라미터에 초기상태를 만들어주는 함수를 넣어주면
+  // 처음 렌더링될 때만 초기상태를 만들어주는 함수를 호출한다.
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBultTodos);
 
   const nextId = useRef(2501);
 
@@ -27,20 +45,16 @@ function App() {
       text: text,
       checked: false,
     };
-    setTodos((todos) => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
   }, []);
 
   const onChecked = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
+    dispatch({ type: 'CHECKED', id });
   }, []);
 
   return (
